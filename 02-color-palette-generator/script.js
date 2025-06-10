03b5c61f6bef04cad769432347c24fb73d7459ac
MaratFa <evolution99@rambler.ru>
2025-06-10 06:59:58 +0300

    Added

---
+++ 02-color-palette-generator/script.js
@@ -0,0 +1,12 @@
+const generateBtn = document.getElementById("generate-btn");
+const paletteContainer = document.querySelector(".palette-container");
+
+generateBtn.addEventListener("click", generatePalette);
+
+paletteContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("copy-btn")) {
+    const hexValue = e.target.previousElementSubling.textContent;
+
+    
+  }
+})
