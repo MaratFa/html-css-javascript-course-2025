bfb11dc5814ea8e99c78aadc53abb66c9777e8b3
MaratFa <evolution99@rambler.ru>
2025-06-14 06:35:22 +0300

    Ediited

--- 04-expense-tracker/script.js
+++ 04-expense-tracker/script.js
@@ -1,14 +1,14 @@
+const balanceEl = document.getelementById("balance");
+const incomeAmountEl = document.getelementById("income-amount");
+const expenseAmountEl = document.getelementById("expense-amount");
+const transactionListEl = document.getelementById("transaction-list");
+const transactionFormEl = document.getelementById("transaction-form");
+const descriptionEl = document.getelementById("description");
+const amountEl = document.getelementById("amount");
 
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
-
-
-
-
-
-
-
-
-
+transactionFormEl.addEventListener("submit", addTransaction);
 
 function addTransaction(e) {
   e.preventDefault();
@@ -18,9 +18,9 @@ function addTransaction(e) {
   const amount = parseFloat(amountEl.value);
 
   transactions.push({
-   id: Date.now(),
-   description,
-   amount,
+    id: Date.now(),
+    description,
+    amount,
   });
 
   localStorage.setItem("transactions", JSON.stringify(transactions));
@@ -28,5 +28,72 @@ function addTransaction(e) {
   updateTransactionList();
   updateSummary();
 
-  transactionFormEl.reset()
+  transactionFormEl.reset();
 }
+
+function updateTransactionList() {
+  transactionListEl.innerHTML = "";
+
+  const sortedTransactions = [...transactions].reverse();
+
+  sortedTransactions.forEach((transaction) => {
+    const transactionEl = createTransactionElement(transaction);
+    transactionListEl.appendChild(transactionEl);
+  });
+}
+
+function createTransactionElement(transaction) {
+  const li = document.createElement("li");
+  li.classList.add("transaction");
+  li.classList.add(transaction.amount > 0 ? "income" : "expense");
+
+  li.innerHTML = `
+    <span>${transaction.description}</span>
+    <span>
+    
+      ${transaction.amount}
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+    </span>
+  `;
+
+  return li;
+}
+
+function updateSummary() {
+  // 100, -50, 200, -200 => 50
+  const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const income = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    // udpate ui => todo: fix the formatting
+    balanceEl.textContent = formatCurrency(balance);
+    incomeAmountEl.textContent = formatCurrency(income);
+    expenseAmountEl.textContent = formatCurrency(expenses);
+}
+
+function formatCurrency(number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(number);
+}
+
+function removeTransaction(id) {
+  // filter out the one we wanted to delete
+  transactions = transactions.filter(transaction => transaction.id !== id)
+
+  localStorage.setItem("transactions", JSON.stringify(transactions))
+
+  updateTransactionList()
+  updateSummary()
+}
+
+// initial render
+updateTransactionList();
+updateSummary();
