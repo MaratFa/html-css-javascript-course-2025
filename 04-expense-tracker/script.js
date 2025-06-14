652cf28c74be8afcfeaca116e18a06b2a3eee915
MaratFa <evolution99@rambler.ru>
2025-06-14 17:08:01 +0300

    Finished

--- 04-expense-tracker/script.js
+++ 04-expense-tracker/script.js
@@ -1,10 +1,10 @@
-const balanceEl = document.getelementById("balance");
-const incomeAmountEl = document.getelementById("income-amount");
-const expenseAmountEl = document.getelementById("expense-amount");
-const transactionListEl = document.getelementById("transaction-list");
-const transactionFormEl = document.getelementById("transaction-form");
-const descriptionEl = document.getelementById("description");
-const amountEl = document.getelementById("amount");
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
 
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
 
@@ -12,11 +12,11 @@ transactionFormEl.addEventListener("submit", addTransaction);
 
 function addTransaction(e) {
   e.preventDefault();
-
+  
   // get form values
   const description = descriptionEl.value.trim();
   const amount = parseFloat(amountEl.value);
-
+  
   transactions.push({
     id: Date.now(),
     description,
@@ -33,9 +33,9 @@ function addTransaction(e) {
 
 function updateTransactionList() {
   transactionListEl.innerHTML = "";
-
+  
   const sortedTransactions = [...transactions].reverse();
-
+  
   sortedTransactions.forEach((transaction) => {
     const transactionEl = createTransactionElement(transaction);
     transactionListEl.appendChild(transactionEl);
@@ -61,7 +61,10 @@ function createTransactionElement(transaction) {
 
 function updateSummary() {
   // 100, -50, 200, -200 => 50
-  const balance = transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
+  const balance = transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
 
   const income = transactions
     .filter((transaction) => transaction.amount < 0)
@@ -71,10 +74,10 @@ function updateSummary() {
     .filter((transaction) => transaction.amount < 0)
     .reduce((acc, transaction) => acc + transaction.amount, 0);
 
-    // udpate ui => todo: fix the formatting
-    balanceEl.textContent = formatCurrency(balance);
-    incomeAmountEl.textContent = formatCurrency(income);
-    expenseAmountEl.textContent = formatCurrency(expenses);
+  // udpate ui => todo: fix the formatting
+  balanceEl.textContent = formatCurrency(balance);
+  incomeAmountEl.textContent = formatCurrency(income);
+  expenseAmountEl.textContent = formatCurrency(expenses);
 }
 
 function formatCurrency(number) {
@@ -86,12 +89,12 @@ function formatCurrency(number) {
 
 function removeTransaction(id) {
   // filter out the one we wanted to delete
-  transactions = transactions.filter(transaction => transaction.id !== id)
+  transactions = transactions.filter((transaction) => transaction.id !== id);
 
-  localStorage.setItem("transactions", JSON.stringify(transactions))
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 
-  updateTransactionList()
-  updateSummary()
+  updateTransactionList();
+  updateSummary();
 }
 
 // initial render
