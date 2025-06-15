5c96f89e028aaddad1821d63b63e98b03897aab4
MaratFa <evolution99@rambler.ru>
2025-06-16 01:15:02 +0300

    Added

---
+++ 07-password-generator/script.js
@@ -0,0 +1,96 @@
+// DOM Elements - all the elements we need from HTML
+const passwordInput = document.getElementById("password");
+const lengthSlider = document.getElementById("length");
+const lengthDisplay = document.getElementById("length-value");
+const uppercaseCheckbox = document.getElementById("uppercase");
+const lowercaseCheckbox = document.getElementById("lowercase");
+const numbersCheckbox = document.getElementById("numbers");
+const symbolsCheckbox = document.getElementById("symbols");
+const generateButton = document.getElementById("generate-btn");
+const copyButton = document.getElementById("copy-btn");
+const strengthBar = document.querySelector("strength-bar");
+const strengthText = document.querySelector("strength-container p");
+
+// Character sets
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberCharacters = "0123456789";
+const symbolCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+
+lengthSlider.addEventListener("input", () => {
+  lengthDisplay.textContent = lengthSlider.value;
+});
+
+generateButton.addEventListener("click", makePassword);
+
+function makePassword() {
+  const length = Number(lengthSlider.value);
+  const includeUppercase = uppercaseCheckbox.checked;
+  const includeLowercase = lowercaseCheckbox.checked;
+  const includeNumbers = numbersCheckbox.checked;
+  const includeSymbols = symbolsCheckbox.checked;
+
+  if (
+    !includeUppercase &&
+    !includeLowercase &&
+    !includeNumbers &&
+    !includeSymbols
+  ) {
+    alert("Please select at least one char type.");
+    return;
+  }
+
+  const newPassword = chreateRandomPassword(
+    length,
+    includeUppercase,
+    includeLowercase,
+    includeNumbers,
+    includeSymbols
+  );
+
+  passwordInput.value = newPassword;
+  updateStrengthMeter(newPassword);
+}
+
+function updateStrengthMeter(newPassword) {
+  const passwordLength = password.length;
+  const hasUppercase = /[A-Z]/.test(password);
+  const hasLowercase = /[a-z]/.test(password);
+  const hasNumbers = /[0-9]/.test(password);
+  const hasSymbols = /!@#$%^&*()-_=+[]{}|;:,.<>?/.test(password);
+
+  let strengthScore = 0;
+
+  // here the .min will get the minimum value
+  // but this will make sure that "at maximum" you would get 40
+  strengthScore += Math.min(passwordLength * 2, 40);
+
+  if (hasUppercase) strengthScore += 15;
+  if (hasLowercase) strengthScore += 15;
+  if (hasNumbers) strengthScore += 15;
+  if (hasSymbols) strengthScore += 15;
+}
+
+function chreateRandomPassword(
+  length,
+  includeUppercase,
+  includeLowercase,
+  includeNumbers,
+  includeSymbols
+) {
+  let allCharacters = "";
+
+  if (includeUppercase) allCharacters += uppercaseLetters;
+  if (includeLowercase) allCharacters += lowercaseLetters;
+  if (includeNumbers) allCharacters += numberCharacters;
+  if (includeSymbols) allCharacters += symbolCharacters;
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[randomIndex];
+  }
+
+  return password;
+}
