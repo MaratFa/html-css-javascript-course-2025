284978a927973b4fd0b66d77f40080b0db60dc81
MaratFa <evolution99@rambler.ru>
2025-07-11 04:57:21 +0300

    Ediited

---
+++ 13-currency-converter/script.js
@@ -0,0 +1,15 @@
+const converterForm = document.getElementById("converter-form");
+const fromCurrency = document.getElementById("from-currency");
+const toCurrency = document.getElementById("to-currency");
+const amountInput = document.getElementById("amount");
+const resultDiv = document.getElementById("result");
+
+window.addEventListener("load", fetchCurrencies);
+
+function fetchCurrencies() {
+  // https://api.exchangerate-api.com/v4/latest/USD
+}
+
+function convertCurrency() {
+  
+}
